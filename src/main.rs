919c2688831ddc42f38a179d99373a60f72cd12b
krisn2 <krishnasarone08@gmail.com>
2025-01-25 23:31:58 +0530

    adding dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY must be set");
     println!("{}", api_key);
+
+    let url = format!(
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={}",
+        api_key
+    );
 }
